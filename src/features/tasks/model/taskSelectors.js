f24486a46f0416fc3b1ebbc1ef5f9d1629a97617
delepover56 <taha82426980@gmail.com>
2026-07-24 02:20:38 +0500

    custom functions

--- src/features/tasks/model/taskSelectors.js
+++ src/features/tasks/model/taskSelectors.js
@@ -1,7 +1,69 @@
 export const selectNonArchivedTasks = (tasks) => {
-    return (
-        tasks.filter((tasks) => !tasks.archived)
-    )
+    const nonArchivedTasks = tasks.filter((task) => {
+        return task.archived === false
+    })
+
+    return nonArchivedTasks
 }
 
-``
+export const selectTodaysTasks = (tasks, todaysDate) => {
+    const nonArchivedTasks = selectNonArchivedTasks(tasks);
+
+    const todaysTasks = nonArchivedTasks.filter((task) => {
+        return task.dueDate === todaysDate && task.completed === false
+    })
+    return todaysTasks
+}
+
+export const selectUpcomingTasks = (tasks, dueDate) => {
+    const nonArchivedTasks = selectNonArchivedTasks(tasks);
+
+    const upcomingTasks = nonArchivedTasks.filter((task) => {
+        return task.dueDate > dueDate && task.completed === false
+    })
+    return upcomingTasks
+}
+
+export const selectHighPriorityTasks = (tasks) => {
+    const nonArchivedTasks = selectNonArchivedTasks(tasks);
+
+    const highPriorityTasks = nonArchivedTasks.filter((task) => {
+        return task.completed === false && task.priority === "High";
+    })
+    return highPriorityTasks
+}
+
+export const selectMediumPriorityTasks = (tasks) => {
+    const nonArchivedTasks = selectNonArchivedTasks(tasks);
+
+    const mediumPriorityTasks = nonArchivedTasks.filter((task) => {
+        return task.completed === false && task.priority === "Medium";
+    })
+    return mediumPriorityTasks
+}
+
+export const selectLowPriorityTasks = (tasks) => {
+    const nonArchivedTasks = selectNonArchivedTasks(tasks);
+
+    const lowPriorityTasks = nonArchivedTasks.filter((task) => {
+        return task.completed === false && task.priority === "Low";
+    })
+    return lowPriorityTasks
+}
+
+export const selectCompletedTasks = (tasks) => {
+    const nonArchivedTasks = selectNonArchivedTasks(tasks);
+
+    const completedTasks = nonArchivedTasks.filter((task) => {
+        return task.completed === true;
+    })
+    return completedTasks
+}
+
+export const selectArchivedTasks = (tasks) => {
+
+    const archivedTasks = tasks.filter((task) => {
+        return task.archived === true;
+    })
+    return archivedTasks
+}
